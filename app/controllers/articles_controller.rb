f6de9fbe2ab3007af78211e097d1dc938a9858c7
dmc2m4 <danielmartinezcabrera@hotmail.com>
2023-03-15 11:27:51 -0500

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,27 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show 
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+    @articles = Article.new(title: "...", body: "...")
+    if @articles.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+
+
+
+end
